afdbecdb1120f603f0c457d75fdf809643fba592
flormaza <florrmazavega@gmail.com>
2023-04-08 12:45:17 -0300

    final front3

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,12 +3,19 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        <p>Powered by</p>
-        <img src='.public\images\DH.png' alt='DH-logo' />
-        <img src='.public\images\ico-facebook.png' alt='Facebook-logo' />
-        <img src='./public/ico-instagram.png.png' alt='Instagram-logo' />
-        <img src='./public/ico-whatsapp.png' alt='whatsapp-logo' />
-    </footer>
+    <div className='footer-paragraph'><p >Powered by</p></div> 
+ 
+     <div className='footer-logo-and-icons'>
+       <img className='footer-logo' src="/images/DH.png" alt="" width={200}/>
+      
+       <div>
+         <a href="/#"><img className='footer-icons' src="/images/ico-facebook.png" alt="" /></a>
+         <a href="/#"><img className='footer-icons' src="/images/ico-instagram.png" alt="" /></a>
+         <a href="/#"><img className='footer-icons' src="/images/ico-whatsapp.png" alt="" /></a>
+         <a href="/#"><img className='footer-icons' src="/images/ico-tiktok.png" alt="" /></a>
+       </div>
+     </div>
+ </footer>
   )
 }
 
